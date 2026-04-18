a910d85445866ad8b5ff7d1c0ed7def25df5d44a
fisaxili <anfisavoro@gmail.com>
2026-04-18 13:17:03 +0300

    Создан класс Graph, добавлены поля и конструктор.

--- Lab04_Variant03/Graph.cs
+++ Lab04_Variant03/Graph.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab04_Variant03
 {
-    internal class Graph
+    /// <summary>
+    /// Представляет граф дорожной сети в виде списка смежности.
+    /// Граф неориентированный, взвешенный.
+    /// </summary>
+    public class Graph
     {
+        // Список смежности: вершина -> список (сосед, вес)
+        private readonly Dictionary<string, List<(string neighbor, double weight)>> _adjacency;
+
+        // Упорядоченный список вершин для стабильного вывода
+        private readonly List<string> _vertices;
+
+        public IReadOnlyList<string> Vertices => _vertices;
+
+        public Graph()
+        {
+            _adjacency = new Dictionary<string, List<(string, double)>>();
+            _vertices = new List<string>();
+        }
     }
 }
